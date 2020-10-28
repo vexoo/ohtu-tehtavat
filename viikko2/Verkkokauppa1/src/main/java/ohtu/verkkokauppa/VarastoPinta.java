649046053a7b8601c4fba2624173acca57799639
Jani Toivanen <jntv90@gmail.com>
2020-10-28 14:31:56 +0200

    viikko2 tehtävät tehty

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoPinta.java
@@ -0,0 +1,13 @@
+package ohtu.verkkokauppa;
+
+public interface VarastoPinta {
+
+    Tuote haeTuote(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+
+    int saldo(int id);
+    
+}
